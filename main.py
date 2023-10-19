66f188f7a475c25892494f7593d1c7986a5a5745
RobelAbr <robel.a@hotmail.de>
2023-10-19 14:30:10 +0200

    added scraper

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
+# Die URL der Webseite, von der du Bilder herunterladen möchtest
+url = 'https://unsplash.com/de/s/fotos/hidden-face'
 
-def create_folder(folder_name):
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
+# Der Name des Ordners, in dem die Bilder gespeichert werden sollen
+folder_name = 'downloaded_images'
 
+# Erstelle den Ordner, wenn er nicht existiert
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
-def download_images(url, folder_name):
-    create_folder(folder_name)
+# User-Agent-Header hinzufügen, um sich als Browser auszugeben
+headers = {'User-Agent': 'Mozilla/5.0'}
 
-    # User-Agent Header hinzufügen, um sich als Browser auszugeben
-    headers = {'User-Agent': 'Mozilla/5.0'}
+# HTTP-Anfrage an die Webseite senden
+response = requests.get(url, headers=headers)
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        images = soup.find_all('img')
+# Überprüfen, ob die Anfrage erfolgreich war (Statuscode 200)
+if response.status_code == 200:
+    # HTML-Inhalt der Webseite analysieren
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-        for i, img in enumerate(images):
-            img_url = img.get('src')
-            if img_url and 'http' in img_url:
-                try:
-                    image_response = requests.get(img_url, headers=headers)
-                    if image_response.status_code == 200:
-                        # Den Dateinamen aus dem Bild-URL extrahieren
-                        image_name = os.path.join(folder_name, f'image_{i + 1}.jpg')
-                        with open(image_name, 'wb') as f:
-                            f.write(image_response.content)
-                        print(f'Bild {i + 1} heruntergeladen: {image_name}')
-                    else:
-                        print(f'Fehler beim Herunterladen von Bild {i + 1}: {img_url}')
-                except Exception as e:
-                    print(f'Fehler beim Herunterladen von Bild {i + 1}: {e}')
-            else:
-                print(f'Ungültige Bild-URL in Bild {i + 1}: {img_url}')
-    else:
-        print(f'Fehler beim Abrufen der Webseite: {url}')
+    # Alle Bilder auf der Webseite finden
+    images = soup.find_all('img')
 
+    # Bilder herunterladen und im Ordner speichern
+    for i, img in enumerate(images):
+        img_url = img.get('src')
+        if img_url and 'http' in img_url:
+            try:
+                # Vollständige Bild-URL erstellen (falls relativ)
+                image_url = urljoin(url, img_url)
 
-if __name__ == '__main__':
-    # URL der Webseite, von der du Bilder herunterladen möchtest
-    url = 'https://www.google.com/search?client=firefox-b-d&sxsrf=APwXEdeh5oSkWy6kFE1kV9GuuGRdjYHfbA:1688026837039&q=hide+face&tbm=isch&sa=X&ved=2ahUKEwjixPj_hej_AhWQSfEDHUrWCHwQ0pQJegQICRAB&biw=1920&bih=955&dpr=1'
+                # Den Dateinamen aus der URL extrahieren
+                image_name = os.path.join(folder_name, f'image_{i + 1}.jpg')
 
-    # Der Name des Ordners, in dem die Bilder gespeichert werden sollen
-    folder_name = 'downloaded_images'
+                # Bild herunterladen und speichern
+                image_data = requests.get(image_url).content
+                with open(image_name, 'wb') as image_file:
+                    image_file.write(image_data)
 
-    download_images(url, folder_name)
+                print(f'Bild {i + 1} heruntergeladen: {image_name}')
+            except Exception as e:
+                print(f'Fehler beim Herunterladen von Bild {i + 1}: {e}')
+        else:
+            print(f'Ungültige Bild-URL in Bild {i + 1}: {img_url}')
+else:
+    print(f'Fehler beim Abrufen der Webseite: {url}')
